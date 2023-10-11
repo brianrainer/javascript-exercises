5b8d32e46d3196baf21d3db49a967f9e26166879
Brian Rainer <brianbulletworks@gmail.com>
2023-10-11 17:49:01 +0700

    Work through exercise 06 and 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (!Number.isInteger(year)) {
+        return 'ERROR';
+    }
+
+    if (year % 400 == 0) {
+        return true;
+    }
+
+    if (year % 100 == 0) {
+        return false;
+    }
+
+    if (year % 4 == 0) {
+        return true;
+    }
+
+    return false;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegree) {
+  return Math.round((fahrenheitDegree - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusDegree) {
+  return (Math.round((celciusDegree) * 9 / 5 * 10) / 10) + 32;
 };
 
 // Do not edit below this line
