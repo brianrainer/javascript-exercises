91e059c242a489faf2d5ffcaf3ed1615dca4ae28
Brian Rainer <brianbulletworks@gmail.com>
2023-10-27 14:25:39 +0700

    Add calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,63 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	let sum = 0;
+  numList.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numList) {
+    if (numList.length === 0) {
+      return 0;
+    }
 
+    let sum = 1;
+    numList.forEach(element => {
+      sum *= element;
+    });
+    return sum;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	if (pow === 0) {
+    return 1;
+  }
+
+  if (pow === 1) {
+    return num;
+  }
+
+  let isNegative = (pow < 0);
+  if (isNegative) {
+    pow = -pow;
+  }
+
+  let sum = 1;
+  for (let i = 0; i < pow; i++) {
+    sum *= num;
+  }
+
+  if (isNegative) {
+    return 1.0 / sum;
+  }
+
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+	for (let i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
