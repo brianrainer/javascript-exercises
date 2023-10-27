20b73fea8f50bd27571c8d4fe79429e7e5e28e86
Brian Rainer <brianbulletworks@gmail.com>
2023-10-27 15:13:37 +0700

    Add fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (isNaN(num)) {
+        return 'OOPS';
+    }
+
+    num = Number(num);
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num < 3) {
+        return 1;
+    }
+
+    let a = 1;
+    let b = 1;
+    for (let i = 0; i < num - 2; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 
 };
 
