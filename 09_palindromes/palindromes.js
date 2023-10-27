4afc55db52c06b7bfdf5b0a3a12048f725c5ea17
Brian Rainer <brianbulletworks@gmail.com>
2023-10-27 14:52:59 +0700

    Add solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // sanitize
+    let sanitize = word.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    let isPalindrome = true;
+    for (let i = 0; i < sanitize.length; i++) {
+        if (sanitize[i] !== sanitize[sanitize.length-1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
